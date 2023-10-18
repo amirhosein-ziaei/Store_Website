4453d78d397bd0c8cb1eafdcb438bf172df7dcd5
amirhosein-ziaei <amir.ziaei1379@gmail.com>
2023-10-18 15:03:42 +0330

    add comment model and register in admin pannel, also add it in product_detail file

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -15,3 +16,23 @@ class Product(models.Model):
     
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.pk])
+    
+    
+
+class Comment(models.Model):
+    PRODUCT_STARS = [
+        ('1', 'very bad'),
+        ('2', 'bad'),
+        ('3', 'normal'),
+        ('4', 'good'),
+        ('5', 'perfect'),
+         
+    ]
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments') # product.comments for getting all the comments of this product
+    body = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments') # when we want to know all the comments of 
+                                                        # a specific author, we can use user.comments (comments is because of related_name)
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True) # it meand by default comments are approved 
