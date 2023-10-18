e00c203d2cfce599eaaf93bdf4e5771a6db4193f
amirhosein-ziaei <amir.ziaei1379@gmail.com>
2023-10-18 22:01:36 +0330

    add tabularinline for comments

--- products/admin.py
+++ products/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 
 from .models import Product, Comment
 
+
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'body', 'stars', 'active',]
+    extra = 1
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active',]
-    
+    inlines = [
+        CommentsInline
+    ]
     
 
 @admin.register(Comment)
